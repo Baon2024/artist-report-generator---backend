9b68ca15f1f2790d9e3e7aa5e4f6424123c85d0e
Joe Joe <baon2020@protonmail.com>
2026-01-27 14:32:13 +0000

    change supabase psycopg2 variable names to avoid server port overlap

--- serverConcurrentTemplate.py
+++ serverConcurrentTemplate.py
@@ -302,22 +302,22 @@ async def report_generator(payload: ReportRequestLatest) -> Any:
 
     #need to get all data from database
     # Fetch variables
-    USER = os.getenv("USER")
-    PASSWORD = os.getenv("PASSWORD")
-    HOST = os.getenv("HOST")
-    PORT = os.getenv("PORT")
-    DBNAME = os.getenv("DBNAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME")
 
     data_for_report = None
 
 
     try:
         with psycopg2.connect(
-            user=USER,
-            password=PASSWORD,
-            host=HOST,
-            port=PORT,
-            dbname=DBNAME
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME
             ) as connection:
             with connection.cursor() as cursor:
                 print("Connection successful!")
